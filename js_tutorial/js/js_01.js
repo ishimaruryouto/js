421e9640925488580cfffadfa7f404097976c754
ishimaruryouto <2240006@ecc.ac.jp>
2025-03-23 23:28:23 +0900

    変数途中

--- js_tutorial/js/js_01.js
+++ js_tutorial/js/js_01.js
@@ -4,3 +4,19 @@ console.log("miyoshinn");
 console.log("5+4");
 console.log(2 + 4);
 
+console.log(8 * 4);
+console.log(24 / 4);
+
+// 文字列
+console.log("ひつじ" + "仙人");
+console.log("20" + "15");
+
+// 変数
+let name = "にんじゃわんこ";
+console.log(name);
+
+// 変数２(円の計算)
+let length = 5;
+console.log(length * length * 3);
+
+
