ab87ca74575b8242698a98cafc78cd5b21f4c194
ishimaruryouto <2240006@ecc.ac.jp>
2025-03-24 22:19:56 +0900

    変数練習

--- js_tutorial/js/js_01.js
+++ js_tutorial/js/js_01.js
@@ -19,4 +19,20 @@ console.log(name);
 let length = 5;
 console.log(length * length * 3);
 
+// 変数の上書き
+name = "うわずきん";
+console.log(name);
+
+// 変数自身の更新
+let number = 7;
+console.log(number);
+
+number = number + 3;
+number = number / 2;
+
+// constの使い方
+const language = "フランス語";
+console.log(language);
+console.log(language + "を話せます");
+
 
