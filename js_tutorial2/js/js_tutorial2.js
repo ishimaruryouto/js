9d91e4c6129747d11c55333fab33cea1917eebb8
ishimaruryouto <2240006@ecc.ac.jp>
2025-04-03 13:42:34 +0900

    配列の使い方

--- js_tutorial2/js/js_tutorial2.js
+++ js_tutorial2/js/js_tutorial2.js
@@ -31,3 +31,28 @@ for (number = 1; number <= 100; number++) {
         console.log(number);
     }
 }
+
+// 配列の使い方
+const animals = ["dog", "cat", "sheep"];
+console.log(animals);
+
+console.log(animals[0]);
+console.log(animals[2]);
+
+animals[2] = "rabbit";
+console.log(animals[2]);
+
+for (let i = 0; i < 3; i++) {
+    console.log(animals[i]);
+};
+
+// lengthの使い方
+console.log(animals.length);
+
+for (let i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
+}
+
+
+
+
