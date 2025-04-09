c92c010672cddb452dedb87a6c7ec87e7e16b9ab
ishimaruryouto <2240006@ecc.ac.jp>
2025-04-09 16:25:05 +0900

    tutorial4途中

---
+++ js_tutorial4/js/js_tutorial4.js
@@ -0,0 +1,17 @@
+// tutorial2の復習
+const animalDate = {
+    name: "レオ",
+    age: 3,
+    greet: () => {
+        console.log("こんにちは！");
+    }
+}
+console.log(animalDate.name);
+animalDate.greet();
+
+// クラス名の使い方
+class Animal { }
+const animal = new Animal();
+console.log(animal);
+
+
